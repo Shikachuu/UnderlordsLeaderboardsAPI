25555db832f5882419e5e7c5bb012911255472ee
Máté Czékus <mate.czekus@jura.hu>
2019-07-23 09:48:28 +0200

    ready and merged the getData function to the index

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-var getData = require('./getData');
 //inject dependencies
 var express = require('express');//express webserver
+var request = require('request');
+var cheerio = require('cheerio');//server side jquery
 var fs = require('fs');
 //write express app
 var app = express();
 
+function getData(callback) {
+    let url = 'https://underlords.com/leaderboard';
+    let players = [];
+    request(url, (error, response, html)=>{
+        if (!error) {
+            var $ = cheerio.load(html);
+            $('.row .player').each((i,elem)=>{
+                players.push($(elem).filter('.player').text());
+            });
+            callback(players);
+        }
+    });
+}
+//console.log(getData());
 app.get('/', (req,res)=>{
     res.setHeader('Content-Type', 'application/json');
-    getData((data)=>{
+    getData(function(data){
         res.send(JSON.stringify(data));
     });
 });
 
-app.get('byRank/:from-:to?', (req,res)=>{
+app.get('/byRank/:from-:to?', (req,res)=>{
     /*if (req.param('to') === ) {
         
     }*/
 });
-app.get('/byName/', (req,res)=>{
+app.get('/byName/:name', (req,res)=>{
 
 });
 
