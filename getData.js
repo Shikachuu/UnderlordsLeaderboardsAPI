5ab47af97858384c6d64e650f0a905bb1613c8cf
Máté Czékus <mate.czekus@jura.hu>
2019-07-26 15:13:58 +0200

    added redis for cache

--- getData.js
+++ getData.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');//server side jquery
 var fs = require('fs');
+
 function getData(callback) {
     let url = 'https://underlords.com/leaderboard';
     let players = [];
